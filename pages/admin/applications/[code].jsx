df24f036894a186d435ab42a6b09905241898d55
muhamedsafwat <musafwat666@gmail.com>
2023-07-24 21:30:39 +0300

    Create placeholder pages

---
+++ pages/admin/applications/[code].jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ApplicationDetails = () => {
+  return <div>applicationDetails</div>;
+};
+
+export default ApplicationDetails;
---
+++ pages/admin/applications/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Applications = () => {
+  return <div>applications</div>;
+};
+
+export default Applications;
